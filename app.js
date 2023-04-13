5d53ab69ea8c8cc55a7e71ad7105c8d9e4b3148d
BryanF1nes <bryanfinesw@gmail.com>
2023-04-12 20:15:30 -0400

    added functionality

--- app.js
+++ app.js
@@ -9,23 +9,65 @@ function Player(name, marker, turn) {
 // Setting up the players
 const player1 = new Player('Bryan', 'X', false);
 const player2 = new Player('Grace', 'O', false);
+const xButton = document.querySelector('.X');
+const oButton = document.querySelector('.O');
+let gameboard = ['', '', '', '', '', '', '', '', ''];
 
-console.log(player1, player2);
+// console.log(player1, player2);
+console.log(gameboard);
 
 // Game logic
-// Player 1 starts so "turn" turns to true.
-// After player 1 places there "marker" set turns to false.
-// Player 2 "turn" is now true and can place there "marker".
 // player wins when they get one of the following --
 // 3 across - horizontally, 3 across vertically, 3 across diagnolly
 // a tie occurs when none of those conditions are met.
 
-function gameLogic(p1, p2) {
-    if (p1.turn == false && p2.turn == false) {
-        p1.turn = true;
-        return {p1, p2};
-    }
-
+function generateGameBoard() {
+    const gameboardDiv = document.querySelector('.gameboard');
+    gameboard.forEach((tile) => {
+        const div = document.createElement('div');
+        div.classList.add('gameboard-tile');
+        gameboardDiv.appendChild(div);
+    })
 }
 
-console.log(gameLogic(player1, player2));
+generateGameBoard();
+
+function gameLogic(p1, p2) {
+    if (p1.turn == true && p2.turn == false) {
+        p2.turn = true;
+        placeMarker(player1);
+    } 
+    if (p1.turn == false && p2.turn == true) {
+        p1.turn = true;
+        placeMarker(player2);
+    }
+}
+
+function placeMarker(player) {
+    const p = document.createElement('p');
+    p.textContent = player.marker;
+    return p;
+}
+
+window.addEventListener('click', (e) => {
+    if (e.target.classList == 'gameboard-tile' && player1.turn == true && player2.turn == false) {
+        gameLogic(player1, player2);
+        e.target.appendChild(placeMarker(player1));
+        player1.turn = false;
+    } else if (e.target.classList == 'gameboard-tile' && player1.turn == false && player2.turn == true) {
+        gameLogic(player1, player2);
+        e.target.appendChild(placeMarker(player2));
+        player2.turn = false;
+    };
+});
+
+xButton.addEventListener('click', () => {
+    player1.turn = true;
+    console.log(player1.turn);
+})
+oButton.addEventListener('click', () => {
+    player2.turn = true;
+})
+
+
+// console.log(gameLogic(player1, player2));
