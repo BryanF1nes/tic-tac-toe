42bc33bcd23565bcb2da0fdc637f3397dc003d70
BryanF1nes <bryanfinesw@gmail.com>
2023-04-14 12:25:11 -0400

    WORKING LOGIC - THANKS JOEY

--- app.js
+++ app.js
@@ -1,73 +1,76 @@
 // Variables
 function Player(name, marker, turn) {
-    this.name = name;
-    this.marker = marker;
-    this.turn = turn;
-    return {name, marker, turn};
+  this.name = name;
+  this.marker = marker;
+  this.turn = turn;
+  return { name, marker, turn };
 }
 
 // Setting up the players
-const player1 = new Player('Bryan', 'X', false);
-const player2 = new Player('Grace', 'O', false);
-const xButton = document.querySelector('.X');
-const oButton = document.querySelector('.O');
-let gameboard = ['', '', '', '', '', '', '', '', ''];
-
-// console.log(player1, player2);
-console.log(gameboard);
-
-// Game logic
-// player wins when they get one of the following --
-// 3 across - horizontally, 3 across vertically, 3 across diagnolly
-// a tie occurs when none of those conditions are met.
+const player1 = new Player("Bryan", "X", false);
+const player2 = new Player("Grace", "O", false);
+const xButton = document.querySelector(".X");
+const oButton = document.querySelector(".O");
+let gameboard = ["", "", "", "", "", "", "", "", ""];
 
 function generateGameBoard() {
-    const gameboardDiv = document.querySelector('.gameboard');
-    gameboard.forEach((tile) => {
-        const div = document.createElement('div');
-        div.classList.add('gameboard-tile');
-        gameboardDiv.appendChild(div);
-    })
+  const gameboardDiv = document.querySelector(".gameboard");
+  let id = 0;
+  gameboard.forEach((tile) => {
+    const div = document.createElement("div");
+    div.classList.add("gameboard-tile");
+    div.id = id++;
+    div.addEventListener("click", (e) => {
+      let divId = e.target.id;
+      gameboard[divId] = player1.turn ? player1.marker : player2.marker;
+      e.target.textContent = gameboard[divId];
+
+      processMove(gameboard[divId]);
+    });
+    gameboardDiv.appendChild(div);
+  });
 }
+
+const processMove = (ch) => {
+  if (checkVictory(ch)) {
+  } else {
+    if (player1.turn) {
+      player1.turn = false;
+      player2.turn = true;
+    } else {
+      player1.turn = true;
+      player2.turn = false;
+    }
+  }
+};
+
+const checkVictory = (ch) => {
+  console.log(gameboard);
+  return (
+    (gameboard[0] == ch && gameboard[1] == ch && gameboard[2] == ch) ||
+    (gameboard[3] == ch && gameboard[4] == ch && gameboard[5] == ch) ||
+    (gameboard[6] == ch && gameboard[7] == ch && gameboard[8] == ch) ||
+    (gameboard[0] == ch && gameboard[3] == ch && gameboard[6] == ch) ||
+    (gameboard[1] == ch && gameboard[4] == ch && gameboard[7] == ch) ||
+    (gameboard[2] == ch && gameboard[5] == ch && gameboard[8] == ch) ||
+    (gameboard[0] == ch && gameboard[4] == ch && gameboard[8] == ch) ||
+    (gameboard[2] == ch && gameboard[4] == ch && gameboard[6] == ch)
+  );
+};
 
 generateGameBoard();
 
-function gameLogic(p1, p2) {
-    if (p1.turn == true && p2.turn == false) {
-        p2.turn = true;
-        placeMarker(player1);
-    } 
-    if (p1.turn == false && p2.turn == true) {
-        p1.turn = true;
-        placeMarker(player2);
-    }
-}
-
-function placeMarker(player) {
-    const p = document.createElement('p');
-    p.textContent = player.marker;
-    return p;
-}
-
-window.addEventListener('click', (e) => {
-    if (e.target.classList == 'gameboard-tile' && player1.turn == true && player2.turn == false) {
-        gameLogic(player1, player2);
-        e.target.appendChild(placeMarker(player1));
-        player1.turn = false;
-    } else if (e.target.classList == 'gameboard-tile' && player1.turn == false && player2.turn == true) {
-        gameLogic(player1, player2);
-        e.target.appendChild(placeMarker(player2));
-        player2.turn = false;
-    };
+xButton.addEventListener("click", () => {
+  if ((player2.turn = true)) {
+    player2.turn = false;
+  }
+  player1.turn = true;
 });
-
-xButton.addEventListener('click', () => {
-    player1.turn = true;
-    console.log(player1.turn);
-})
-oButton.addEventListener('click', () => {
-    player2.turn = true;
-})
-
+oButton.addEventListener("click", () => {
+  if ((player1.turn = true)) {
+    player1.turn = false;
+  }
+  player2.turn = true;
+});
 
 // console.log(gameLogic(player1, player2));
